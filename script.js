93ccf7c47444cbe27124bd2ba5b1b5be63f7e678
KaileyLiou <kaileyliou09@gmail.com>
2025-07-27 14:46:25 -0400

    added html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var Typed = new Typed(".typing-header", {
+var typed = new Typed(".typing-header", {
     strings: ["Fight Food Waste. Build a Sustainable Future."],
     typeSpeed: 100,
-    backspeed: 100,
+    backSpeed: 100,
     loop: true
 });
