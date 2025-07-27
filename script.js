68d0b3851c755ff8b46f0eff3001ca985b73f6eb
KaileyLiou <kaileyliou09@gmail.com>
2025-07-27 19:54:38 -0400

    added personalization

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ var typed = new Typed(".typing-header", {
     backSpeed: 100,
     loop: true
 });
+
+var displayName = document.getElementById("name");
+var button = document.getElementById("myButton");
+
+button.addEventListener("click", showPledge);
+
+function showPledge() {
+  var text = document.getElementById("myText").value;
+  displayName.innerHTML = "I, " + text + " am committing to reducing food waste. This week, I will try to not waste " + food + " , and I will do that by " + action + ".";
+}
