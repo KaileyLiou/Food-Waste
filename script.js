4a7810ffe94c3211d6dab67d352ab1164093ce36
KaileyLiou <kaileyliou09@gmail.com>
2025-07-27 22:04:00 -0400

    updated css

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// var typed = new Typed(".typing-header", {
-//     strings: ["Fight Food Waste. Build a Sustainable Future."],
-//     typeSpeed: 100,
-//     backSpeed: 100,
-//     loop: true
-// });
-
 var typingElement = document.querySelector(".typing-header");
 if (typingElement) {
     var Typed = new Typed(".typing-header", {
@@ -27,5 +20,5 @@ function showPledge() {
     var food = document.getElementById("food").value;
     var action = document.getElementById("action").value;
     
-    displayPledge.innerHTML = "I, " + displayName + " am committing to reducing food waste. This week, I will try to not waste " + food + " , and I will do that by " + action + ".";
+    displayPledge.innerHTML = "I, " + displayName + ", am committing to reducing food waste. This week, I will try to not waste " + food + ", and I will do that by " + action + ".";
 }
