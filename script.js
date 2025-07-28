74f2b59562fc9dfff82d689974e4967de335e51c
KaileyLiou <kaileyliou09@gmail.com>
2025-07-27 23:12:09 -0400

    added facts

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ function showPledge() {
     
     displayPledge.innerHTML = "I, " + displayName + ", am committing to reducing food waste. This week, I will try to not waste " + food + ", and I will do that by " + action + ".";
 }
+
+var factList = [
+  "More than 80 percent of Americans discard perfectly good food because they misunderstand expiration labels.",
+  "Wasting food contributes to 11 percent of the world’s greenhouse gas emissions.",
+  "Nearly 35 million people across America live with food insecurity. 10 million of them are children.",
+    "Americans discard more food than any other country, nearly 40 million tons — or 30-40 percent of the entire US food supply.",
+    "One-third of all food produced around the world is wasted.",
+];
+
+var fact = document.getElementById("fact");
+var myButton = document.getElementById("myButton");
+var count = 0;
+
+if (myButton) {
+  myButton.addEventListener("click", displayFact);
+}
+
+function displayFact() {
+  fact.innerHTML = factList[count];
+  count++;
+  if(count == factList.length) {
+    count = 0;
+  }
+}
